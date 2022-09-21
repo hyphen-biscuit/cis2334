c30355e33de2584ccecf1ec2c825267db1f6ce72
dashwhen <37599589+dashwhen@users.noreply.github.com>
2022-09-21 15:42:38 -0500

    Homework 2 Programs

---
+++ Homework2/Lab 1-12/main.py
@@ -0,0 +1,4 @@
+#Dashiell Wendt 2033998
+userNum = int(input())
+userNumSquared = userNum * userNum
+print(userNumSquared)
---
+++ Homework2/Lab 1-20/main.py
@@ -0,0 +1,8 @@
+# Dashiell Wendt 2033998
+square1 = int(input("Enter integer:\n"))
+print(f"You entered: {square1}")
+print(f"{square1} squared is {square1**2}")
+print(f"And {square1} cubed is {square1**3} !!")
+square2 = int(input("Enter another integer:\n"))
+print(f"{square1} + {square2} is {square1 + square2}")
+print(f"{square1} * {square2} is {square1 * square2}")
